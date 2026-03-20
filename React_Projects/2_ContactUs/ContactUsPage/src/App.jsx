631b11ecdaaed92429bd58e3319e32b89502cef3
ag20373 <ag20373@gmail.com>
2026-03-21 01:12:37 +0530

    New Project 2

---
+++ React_Projects/2_ContactUs/ContactUsPage/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Navigation from "./Components/Navigation/Navigation";
+import ContactHeader from "./Components/ContactHeader/ContactHeader";
+
+function App() {
+  return(
+    <div>
+      <Navigation />
+      <ContactHeader />
+    </div>
+  );
+};
+
+export default App;
---
+++ React_Projects/2_ContactUs/ContactUsPage/src/Components/ContactForm/ContactForm.jsx
@@ -0,0 +1,15 @@
+import sytles from "./Contact.module.css"
+const ContactForm = () => {
+  return (
+    <section className={sytles.container}>
+        <div className={sytles.contact_form}>
+
+        </div>
+        <div className={sytles.contact_image}>
+
+        </div>
+    </section>
+  );
+};
+
+export default ContactForm
